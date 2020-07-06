a72cc36568386724f87ea90b53c4cf327b17294f
961644045 <45554297+961644045@users.noreply.github.com>
2020-07-06 21:14:24 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,21 +1,10 @@
 #!/bin/bash
 #用户输入
 echo
-read -p "输入你的sspanel_url:" sspanelurl
-echo
-echo "-----$sspanelurl-----"
-echo
+read -p "输入你的sspanel_url:" myurl
 read -p "输入你的mukey：" mukey
-echo
-echo "-----$mukey-----"
-echo
 read -p "输入你node_id：" myid
-echo
-echo "-----$myid-----"
-echo
 read -p "输入你的api端口：" myapiport 
-echo
-echo "-----$myapiport-----"
 mkdir v2;cd v2
 #安装docker
 curl -sSL https://get.docker.com | bash
@@ -26,7 +15,7 @@ chmod +x /usr/local/bin/docker-compose
 curl -L https://raw.githubusercontent.com/docker/compose/1.8.0/contrib/completion/bash/docker-compose > /etc/bash_completion.d/docker-compose
 clear
 wget -L https://raw.githubusercontent.com/961644045/ricotest/master/docker-compose.yml
-sed -i "s|sspanel_url:.*|sspanel_url: '${sspanelurl}'|" ./docker-compose.yml
+sed -i "s/sspanel_url:*/sspanel_url: '${myurl}'/g" ./docker-compose.yml
 sed -i "s/key:.*/key: '$mukey'/g" ./docker-compose.yml
 sed -i "s/node_id:.*/node_id: $myid/g" ./docker-compose.yml
 sed -i "s/api_port:.*/api_port: $myapiport/g" ./docker-compose.yml
