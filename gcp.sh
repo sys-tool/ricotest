6cb32bb45b3621899899bc3cef9d86c412d5b0a4
961644045 <45554297+961644045@users.noreply.github.com>
2020-07-07 00:16:09 +0800

    Create gcp.sh

---
+++ gcp.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+echo "本脚本为gcp一键开启root账户登录"
+echo
+echo "晨钟暮雨制作"
+sed -i 's/#Port 22/Port 22/g' /etc/ssh/sshd_config
+sed -i 's/PermitRootLogin no/PermitRootLogin yes/g' /etc/ssh/sshd_config
+sed -i 's/PasswordAuthentication no/#PasswordAuthentication no/g' /etc/ssh/sshd_config
+echo
+#
+sleep 1
+/etc/init.d/ssh restart
+service sshd restart
+systemctl restart sshd
+systemctl restart ssh
+ervice ssh restart
